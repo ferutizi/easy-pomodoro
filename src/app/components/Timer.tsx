0759c26009c4d36b08f6831478c4346c2000e395
ferutizi <fer.utizi@gmail.com>
2023-11-11 15:58:23 -0300

    custom variable css for animation-duration, function totaTime in seconds, useMemo

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -1,13 +1,23 @@
 'use client'
 
 import './Timer.css'
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useMemo} from 'react';
+import 'react';
+
+declare module 'react' {
+    interface CSSProperties {
+        [key: `--${string}`]: string | number
+    }
+}
 
 export default function Timer() {
-    const [minutes, setMinutes] = useState<number>(0);
+    const [minutes, setMinutes] = useState<number>(1);
     const [seconds, setSeconds] = useState<number>(5);
     const [isRunning, setIsRunning] = useState<boolean>(false);
-    const [circleProgress, setCircleProgress] = useState<boolean>(false)
+    const [circleProgress, setCircleProgress] = useState<boolean>(false);
+    const [totalTime, setTotalTime] = useState<number>(() => minutes * 60 + seconds);
+/* 
+    setTotalTime(useMemo(() => minutes * 60 + seconds, [])); */
   
     useEffect(() => {
         if(isRunning) {
@@ -30,12 +40,20 @@ export default function Timer() {
         }
     }, [minutes, seconds, isRunning]);
 
+    const reset = () => {
+      setTotalTime(10)
+      console.log(totalTime)
+      setMinutes(0)
+      setSeconds(10)
+      setCircleProgress(false)
+    }
+
     return (
       <>
         <svg width="300" height="300">
           <circle className='base__circle' r="130" cx="50%" cy="50%" pathLength="100" />
           { circleProgress ?
-            <circle className='progress__circle' r="124" cx="50%" cy="50%" pathLength="100" />
+            <circle style={{"--totalTime": `${totalTime}s`}} className='progress__circle' r="124" cx="50%" cy="50%" pathLength="100" />
             : null
           }
         </svg>
@@ -44,6 +62,7 @@ export default function Timer() {
             {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
           </p>
           <button type='button' onClick={()=> {setIsRunning(true), setCircleProgress(true)}}>Iniciar</button>
+          <button type='button' onClick={()=> reset()}>Reiniciar</button>
         </div>
       </>
     );
