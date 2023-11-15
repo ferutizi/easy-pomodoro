895f3813a734b48cc4aabe32f003dfb0b1a33e22
ferutizi <fer.utizi@gmail.com>
2023-11-15 02:15:06 -0300

    Added initial time state, intervalState(pomodoro or break), number of pomodoro are completed. Function reset and next

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -6,24 +6,31 @@ import 'react';
 
 declare module 'react' {
   interface CSSProperties {
-      [key: `--${string}`]: string | number
+    [key: `--${string}`]: string | number
   }
 }
 
 export default function Timer() {
-  const [minutes, setMinutes] = useState<number>(1);
-  const [seconds, setSeconds] = useState<number>(5);
+  //Initial pomodoro time
+  const [pomodoroMinutes, setPomodoroMinutes] = useState(0);
+  const [pomodoroSeconds, setPomodoroSeconds] = useState(12);
+  //Initial break time
+  const [breakMinutes, setBreakMinutes] = useState(0);
+  const [breakSeconds, setBreakSeconds] = useState(10);
+  //State pomodoro or break
+  const [intervalState, setIntervalState] = useState<boolean>(true);
+  const [minutes, setMinutes] = useState<number>(pomodoroMinutes);
+  const [seconds, setSeconds] = useState<number>(pomodoroSeconds);
   const [isRunning, setIsRunning] = useState<boolean>(false);
   const [circleProgress, setCircleProgress] = useState<boolean>(false);
   const [totalTime, setTotalTime] = useState<number>();
+  const [completedPomodoro, setCompletedPomodoro] = useState<number>(0);
 
   useEffect(() => {
     if(isRunning) {
       setTotalTime(minutes * 60 + seconds);
     }
-  },[isRunning])
-
-  
+  },[isRunning]);
 
   useEffect(() => {
       if(isRunning) {
@@ -33,6 +40,7 @@ export default function Timer() {
                 setIsRunning(false);
                 setCircleProgress(false);
                 clearInterval(countDown);
+                setCompletedPomodoro(completedPomodoro + 1)
                 return;
               }
               setSeconds(59);
@@ -45,18 +53,26 @@ export default function Timer() {
             clearInterval(countDown);
           };
       }
-  }, [minutes, seconds, isRunning]);
+  }, [minutes, seconds, isRunning, completedPomodoro]);
 
   const reset = () => {
-    console.log(totalTime)
-    setIsRunning(false)
-    setCircleProgress(false)
-    setMinutes(0)
-    setSeconds(10)
+    if(!isRunning) {
+      setMinutes(pomodoroMinutes);
+      setSeconds(pomodoroSeconds);
+      setCompletedPomodoro(0);
+    }
+  }
+
+  const next = () => {
+    setMinutes(breakMinutes);
+    setSeconds(breakSeconds);
+    //inicio descanso
   }
 
   return (
     <>
+      <h2>{intervalState ? 'Pomodoro' : 'Descanso'}</h2>
+      <h2>{completedPomodoro}</h2>
       <svg width="300" height="300">
         <circle className='base__circle' r="130" cx="50%" cy="50%" pathLength="100" />
         { circleProgress ?
@@ -70,6 +86,7 @@ export default function Timer() {
         </p>
         <button type='button' onClick={()=> {setIsRunning(true), setCircleProgress(true)}}>Iniciar</button>
         <button type='button' onClick={()=> reset()}>Reiniciar</button>
+        <button type='button' onClick={()=> next()}>Continuar</button>
       </div>
     </>
   );
