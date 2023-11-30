5045ad5b196cfad3cf14a75bf11fc6e5258743e6
ferutizi <fer.utizi@gmail.com>
2023-11-30 18:10:06 -0300

    Added font family, styled timer position

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import '../sass/_variables.scss';
 import './Timer.scss'
 import {useState, useEffect} from 'react';
 import 'react';
@@ -13,7 +14,7 @@ declare module 'react' {
 export default function Timer() {
   //Initial interval times
   const [pomodoroMinutes, setPomodoroMinutes] = useState<number>(1);
-  const [pomodoroSeconds, setPomodoroSeconds] = useState<number>(5);
+  const [pomodoroSeconds, setPomodoroSeconds] = useState<number>(50);
   const [breakMinutes, setBreakMinutes] = useState<number>(0);
   const [breakSeconds, setBreakSeconds] = useState<number>(5);
   const [longBreakMinutes, setLongBreakMinutes] = useState<number>(0);
@@ -126,17 +127,26 @@ export default function Timer() {
       </h2>
       <h2>{intervalState ? 'Pomodoro' : 'Descanso'}</h2>
       <h2>{currentPomodoro}</h2>
-      <svg width="300" height="300">
-        <circle className='base__circle' r="130" cx="50%" cy="50%" pathLength="100" />
-        { circleProgress ?
-          <circle style={{"--totalTime": `${totalTime}s`, /* "--steps": `${totalTime}`, */ "--pause": `${animationPause ? 'running' : 'paused'}`}} className='progress__circle' r="130" cx="50%" cy="50%" pathLength="100" />
-          : null
-        }
-      </svg>
+      <div className='timer--clock'>
+        <svg width="300" height="300">
+          <circle className='base__circle' r="130" cx="50%" cy="50%" pathLength="100" />
+          { circleProgress ?
+            <circle style={{"--totalTime": `${totalTime}s`, /* "--steps": `${totalTime}`, */ "--pause": `${animationPause ? 'running' : 'paused'}`}} className='progress__circle' r="130" cx="50%" cy="50%" pathLength="100" />
+            : null
+          }
+        </svg>
+        <div className='timer--minutes'>
+          <p>
+            {minutes.toString().padStart(2, '0')}
+          </p>
+        </div>
+        <div className='timer--seconds'>
+          <p>
+            {seconds.toString().padStart(2, '0')}
+          </p>
+        </div>
+      </div>
       <div>
-        <p>
-          {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
-        </p>
         {
           startButton
           ? <button className='button--start' type='button' onClick={()=> start()}>●</button>
