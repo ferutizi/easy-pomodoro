78e5d430bcd7e3d0d60f5a4b92accc45607b7518
ferutizi <fer.utizi@gmail.com>
2023-12-04 02:00:05 -0300

    changed some scss classes for tailwind styles

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -128,11 +128,11 @@ export default function Timer({theme, setTheme}) {
           <p>Q</p>
         </div>
         <div className='flex justify-center'>
-          <div className='flex'>
-            <div className={`current--pomodoro ${currentPomodoro > 0 ? `${color}-primary` : `${color}-light`}`}></div>
-            <div className={`current--pomodoro ${currentPomodoro > 1 ? `${color}-primary` : `${color}-light`}`}></div>
-            <div className={`current--pomodoro ${currentPomodoro > 2 ? `${color}-primary` : `${color}-light`}`}></div>
-            <div className={`current--pomodoro ${currentPomodoro > 3 ? `${color}-primary` : `${color}-light`}`}></div>
+          <div className='flex w-44 justify-between'>
+            <div className={`shadow-custom w-9 h-9 rounded-full ${currentPomodoro > 0 ? `${color}-primary` : `${color}-light`}`}></div>
+            <div className={`shadow-custom w-9 h-9 rounded-full ${currentPomodoro > 1 ? `${color}-primary` : `${color}-light`}`}></div>
+            <div className={`shadow-custom w-9 h-9 rounded-full ${currentPomodoro > 2 ? `${color}-primary` : `${color}-light`}`}></div>
+            <div className={`shadow-custom w-9 h-9 rounded-full ${currentPomodoro > 3 ? `${color}-primary` : `${color}-light`}`}></div>
           </div>
         </div>
       </section>
