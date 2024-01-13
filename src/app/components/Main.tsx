b8761526c321da27ce9e71e2f816e3edd8f71fab
ferutizi <fer.utizi@gmail.com>
2024-01-13 18:03:35 -0300

    Fixed setSeconds to 0 when timer resets. Added modal configuration and styles

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -44,7 +44,7 @@ export default function Main() {
   }
 
   return(
-    <main className={`flex justify-center ${color}-background h-full items-center`}>
+    <main className={`flex justify-center ${color}-background h-full items-center relative`}>
       <div className={`flex h-screen max-w-sm flex-col items-center justify-between p-5 ${color}-background max-h-hd`}>
       <section className='flex flex-col w-full justify-around gap-10'>
         <div className='flex w-full justify-between'>
@@ -55,25 +55,36 @@ export default function Main() {
         </div>
       </section>
       {modal && 
-        <div className="flex flex-col">
-          <h2 className="text-white">Timer</h2>
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <div className="flex flex-col gap-8">
-              <div className="flex bg-gray-800 h-12 px-2 rounded">
-                <label className="flex items-center justify-between text-lg w-64 text-white"> Pomodoros
-                  <input className="w-20 h-10 text-3xl text-black rounded text-center" name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} />
+        <form onSubmit={handleSubmit} className={`flex flex-col gap-4 text-lg ${color}-secondary modal w-80 p-4 h-5/6 rounded-lg justify-between`}>
+          <div> {/* Header */}
+            <p onClick={() => {setModal(false), setTimerValue(initialTimer)}} className="absolute right-0 px-4 text-white cursor-pointer">X</p>
+            <h2 className="text-white text-lg">Configuration</h2>
+          </div>
+          <div className="flex flex-col gap-4"> {/* Options */}
+            <div className= "flex flex-col gap-4">
+              <h2 className="text-white text-lg">Timer</h2>
+              <div style={{boxShadow: "0px 4px 0.7px rgba(0, 0, 0, 0.25);"}} className={`flex items-center p-2 rounded-lg ${color}-primary`}>
+                <label className="flex items-center justify-between w-64 text-white"> Pomodoros
+                  <input className={`w-20 h-10 text-3xl text-black rounded text-center ps-3 ${color}-light`} name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} />
                 </label>
               </div>
-              <label className="flex items-center justify-between text-lg text-white"> Breaks
-                <input className="w-20 h-12 text-3xl text-black" name="break" type="number" value={timerValue.break} onChange={handleChange} />
-              </label>
-              <label className="flex items-center justify-between text-lg text-white"> Long breaks
-                <input className="w-20 h-12 text-3xl text-black" name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} />
-              </label>
+              <div className={`flex items-center p-2 rounded-lg ${color}-primary`}>
+                <label className="flex items-center justify-between w-64 text-white"> Breaks
+                  <input className={`w-20 h-10 text-3xl text-black rounded text-center ps-3 ${color}-light`} name="break" type="number" value={timerValue.break} onChange={handleChange} />
+                </label>
+              </div>
+              <div className={`flex items-center p-2 rounded-lg ${color}-primary`}>
+                <label className="flex items-center justify-between w-64 text-white"> Long breaks
+                  <input className={`w-20 h-10 text-3xl text-black rounded text-center ps-3 ${color}-light`} name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} />
+                </label>
+              </div>
             </div>
-            <button type="submit">ok</button>
-          </form>
-        </div>
+            <div> {/* Footer */}
+              <h2 className="text-white text-lg">Sounds</h2>
+            </div>
+          </div>
+          <button className={`p-2 rounded-lg ${color}-light font-bold text-black`} type="submit">Save changes</button>
+        </form>
       }
       <Timer pomodoroMinutes={pomodoroMinutes} breakMinutes={breakMinutes} longBreakMinutes={longBreakMinutes} />
       </div>
