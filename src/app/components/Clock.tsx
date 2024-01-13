db67654a31982cccf8dc0d9dd29d25b7640a6dd1
ferutizi <fer.utizi@gmail.com>
2024-01-13 15:47:37 -0300

    changed ident spaces to 2 in Clock.tsx and Main.tsx

--- src/app/components/Clock.tsx
+++ src/app/components/Clock.tsx
@@ -2,41 +2,41 @@ import './Clock.scss';
 import { Theme } from "../context/ThemeContext";
 
 interface ClockProps {
-    color: Theme,
-    circleProgress: boolean,
-    totalTime: number | undefined,
-    animationPause: boolean,
-    minutes: number,
-    seconds: number
+  color: Theme,
+  circleProgress: boolean,
+  totalTime: number | undefined,
+  animationPause: boolean,
+  minutes: number,
+  seconds: number
 }
 
 export default function Clock({color, circleProgress, totalTime, animationPause, minutes, seconds}: ClockProps) {
-    return(
-        <section className='timer--clock'>
-            <svg width="300" height="300">
-            <circle className={`base__circle ${color}-secondary`} r="130" cx="50%" cy="50%" pathLength="100" />
-            { circleProgress ?
-                <circle 
-                    style={{"--totalTime": `${totalTime}s`, /* "--steps": `${totalTime}`, */ "--pause": `${animationPause ? 'running' : 'paused'}`}} 
-                    className={`progress__circle ${color}-secondary ${color}-primary`} 
-                    r="130" 
-                    cx="50%" 
-                    cy="50%" 
-                    pathLength="100"
-                />
-                : null
-            }
-            </svg>
-            <div className='timer--minutes'>
-                <p>
-                    {minutes.toString().padStart(2, '0')}
-                </p>
-            </div>
-            <div className='timer--seconds'>
-                <p>
-                    {seconds.toString().padStart(2, '0')}
-                </p>
-            </div>
-        </section> 
-    );
+  return(
+    <section className='timer--clock'>
+      <svg width="300" height="300">
+      <circle className={`base__circle ${color}-secondary`} r="130" cx="50%" cy="50%" pathLength="100" />
+      {circleProgress ?
+        <circle 
+          style={{"--totalTime": `${totalTime}s`, /* "--steps": `${totalTime}`, */ "--pause": `${animationPause ? 'running' : 'paused'}`}} 
+          className={`progress__circle ${color}-secondary ${color}-primary`} 
+          r="130" 
+          cx="50%" 
+          cy="50%" 
+          pathLength="100"
+        />
+        : null
+      }
+      </svg>
+      <div className='timer--minutes'>
+        <p>
+          {minutes.toString().padStart(2, '0')}
+        </p>
+      </div>
+      <div className='timer--seconds'>
+        <p>
+          {seconds.toString().padStart(2, '0')}
+        </p>
+      </div>
+    </section> 
+  );
 }
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -7,76 +7,76 @@ import { Config } from '../components/svgs';
 import { salsa } from '../fonts';
 
 export default function Main() {
-    const initialTimer = {
-        pomodoro: 25,
-        break: 5,
-        longBreak: 15,
-    }
-    const [timerValue, setTimerValue] = useState<any>(initialTimer);
-    const [pomodoroMinutes, setPomodoroMinutes] = useState<number>(25);
-    const [breakMinutes, setBreakMinutes] = useState<number>(5);
-    const [longBreakMinutes, setLongBreakMinutes] = useState<number>(15);
-    const [modal, setModal] = useState<boolean>(false);
+  const initialTimer = {
+    pomodoro: 25,
+    break: 5,
+    longBreak: 15,
+  }
+  const [timerValue, setTimerValue] = useState<any>(initialTimer);
+  const [pomodoroMinutes, setPomodoroMinutes] = useState<number>(25);
+  const [breakMinutes, setBreakMinutes] = useState<number>(5);
+  const [longBreakMinutes, setLongBreakMinutes] = useState<number>(15);
+  const [modal, setModal] = useState<boolean>(false);
 
-    const themeContext = useContext(ThemeContext);
-    if (!themeContext) {
-      return null;
-    }
+  const themeContext = useContext(ThemeContext);
+  if (!themeContext) {
+    return null;
+  }
 
-    const { theme } = themeContext;
-    const color = theme.color;
-    
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setTimerValue({
-            ...timerValue,
-            [e.target.name]: e.target.value
-        })
-    }
+  const { theme } = themeContext;
+  const color = theme.color;
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTimerValue({
+      ...timerValue,
+      [e.target.name]: e.target.value
+    })
+  }
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if(timerValue.pomodoro > 0) {
-            setPomodoroMinutes(timerValue.pomodoro);
-            setBreakMinutes(timerValue.break);
-            setLongBreakMinutes(timerValue.longBreak);
-        }
-        setModal(false);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(timerValue.pomodoro > 0) {
+      setPomodoroMinutes(timerValue.pomodoro);
+      setBreakMinutes(timerValue.break);
+      setLongBreakMinutes(timerValue.longBreak);
     }
+    setModal(false);
+  }
 
-    return(
-        <main className={`flex justify-center ${color}-background h-full items-center`}>
-            <div className={`flex h-screen max-w-sm flex-col items-center justify-between p-5 ${color}-background max-h-hd`}>
-            <section className='flex flex-col w-full justify-around gap-10'>
-                <div className='flex w-full justify-between'>
-                    <h1 className={`${salsa.className} antialiased text-2xl text-white`} >Easy pomodoro</h1>
-                    <button aria-label='Configuration' onClick={() => setModal(true)}>
-                        <Config className="config" />
-                    </button>
-                </div>
-            </section>
-            {modal && 
-                <div className="flex flex-col">
-                    <h2 className="text-white">Timer</h2>
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                        <div className="flex flex-col gap-8">
-                            <div className="flex bg-gray-800 h-12 px-2 rounded">
-                                <label className="flex items-center justify-between text-lg w-64 text-white"> Pomodoros
-                                    <input className="w-20 h-10 text-3xl text-black rounded text-center" name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} />
-                                </label>
-                            </div>
-                            <label className="flex items-center justify-between text-lg text-white"> Breaks
-                                <input className="w-20 h-12 text-3xl text-black" name="break" type="number" value={timerValue.break} onChange={handleChange} />
-                            </label>
-                            <label className="flex items-center justify-between text-lg text-white"> Long breaks
-                                <input className="w-20 h-12 text-3xl text-black" name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} />
-                            </label>
-                        </div>
-                        <button type="submit">ok</button>
-                    </form>
-                </div>
-            }
-            <Timer pomodoroMinutes={pomodoroMinutes} breakMinutes={breakMinutes} longBreakMinutes={longBreakMinutes} />
+  return(
+    <main className={`flex justify-center ${color}-background h-full items-center`}>
+      <div className={`flex h-screen max-w-sm flex-col items-center justify-between p-5 ${color}-background max-h-hd`}>
+      <section className='flex flex-col w-full justify-around gap-10'>
+        <div className='flex w-full justify-between'>
+          <h1 className={`${salsa.className} antialiased text-2xl text-white`} >Easy pomodoro</h1>
+          <button aria-label='Configuration' onClick={() => setModal(true)}>
+            <Config className="config" />
+          </button>
+        </div>
+      </section>
+      {modal && 
+        <div className="flex flex-col">
+          <h2 className="text-white">Timer</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div className="flex flex-col gap-8">
+              <div className="flex bg-gray-800 h-12 px-2 rounded">
+                <label className="flex items-center justify-between text-lg w-64 text-white"> Pomodoros
+                  <input className="w-20 h-10 text-3xl text-black rounded text-center" name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} />
+                </label>
+              </div>
+              <label className="flex items-center justify-between text-lg text-white"> Breaks
+                <input className="w-20 h-12 text-3xl text-black" name="break" type="number" value={timerValue.break} onChange={handleChange} />
+              </label>
+              <label className="flex items-center justify-between text-lg text-white"> Long breaks
+                <input className="w-20 h-12 text-3xl text-black" name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} />
+              </label>
             </div>
-        </main>
-    );
+            <button type="submit">ok</button>
+          </form>
+        </div>
+      }
+      <Timer pomodoroMinutes={pomodoroMinutes} breakMinutes={breakMinutes} longBreakMinutes={longBreakMinutes} />
+      </div>
+    </main>
+  );
 }
