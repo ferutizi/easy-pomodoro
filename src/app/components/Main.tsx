afcc81cdd97249faabf033987eff09fc46c3b442
ferutizi <fer.utizi@gmail.com>
2024-01-14 19:03:26 -0300

    styles settings, animations

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -57,40 +57,41 @@ export default function Main() {
       {modal && 
         <form onSubmit={handleSubmit} className={`flex flex-col text-lg ${color}-primary modal w-80 p-4 h-5/6 rounded-lg justify-between`}>
           <div> {/* Header */}
-            <p onClick={() => {setModal(false), setTimerValue(initialTimer)}} className="absolute right-0 px-4 text-white cursor-pointer">X</p>
+            <div className={`absolute right-4 py-0 px-2 rounded-full text-white cursor-pointer hover:bg-white hover:text-black transition-all ease-in duration-300`}>
+              <span onClick={() => {setModal(false), setTimerValue(initialTimer)}}>X</span>
+            </div>
             <h2 className="text-white text-lg">Settings</h2>
-{/*             <hr className="mt-3"></hr> */}
           </div>
           <div className="flex flex-col gap-6"> {/* Options */}
             <div className= "flex flex-col gap-4">
               <div>
                 <h2 className="text-white text-center text-xl pl-0">Timer</h2>
-                <hr style={{borderColor: "#ffa8a8"}}></hr>
+                <hr style={{borderColor: "white"}}></hr>
               </div>
               <div className="flex flex-col items-center">
                 <div className={`flex items-center p-2 ${color}-primary`}>
-                  <label className="flex items-center justify-between w-64 text-base text-white"> Pomodoros
-                    <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light`} name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} min={1} max={120} />
+                  <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Pomodoros
+                    <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} min={1} max={120} />
                   </label>
                 </div>
                 <div className={`flex items-center p-2 ${color}-primary`}>
-                  <label className="flex items-center justify-between w-64 text-base text-white"> Breaks
-                    <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light`} name="break" type="number" value={timerValue.break} onChange={handleChange} min={1} max={120} />
+                  <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Breaks
+                    <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="break" type="number" value={timerValue.break} onChange={handleChange} min={1} max={120} />
                   </label>
                 </div>
                 <div className={`flex items-center p-2 ${color}-primary`}>
-                  <label className="flex items-center justify-between w-64 text-base text-white"> Long breaks
-                    <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light`} name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} min={1} max={120} />
+                  <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Long breaks
+                    <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} min={1} max={120} />
                   </label>
                 </div>
               </div>
             </div>
-            <div>
+            <div> 
               <h2 className="text-white text-center text-xl pl-0">Sounds</h2>
-              <hr style={{borderColor: "#ffa8a8"}}></hr>
+              <hr style={{borderColor: "white"}}></hr>
             </div>
           </div> {/* Footer */}
-          <button className={`p-2 rounded-lg ${color}-light font-bold text-black`} type="submit">Save changes</button>
+          <button className={`p-2 rounded-lg ${color}-light font-bold text-black hover:bg-white transition-all ease-out 2seg duration-700`} type="submit">Save changes</button>
         </form>
       }
       <Timer pomodoroMinutes={pomodoroMinutes} breakMinutes={breakMinutes} longBreakMinutes={longBreakMinutes} />
