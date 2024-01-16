9350c8038f072bbe2b21185f23ae2f87ac860849
ferutizi <fer.utizi@gmail.com>
2024-01-16 16:41:23 -0300

    animated arrow icon

--- src/app/components/Themes.tsx
+++ src/app/components/Themes.tsx
@@ -17,16 +17,16 @@ export default function Themes() {
 
     return(
         <div className='flex flex-col items-center'>
-          <button aria-label={`${showThemes ? 'Hide color themes' : 'Show color themes'}`} onClick={() => setShowThemes(!showThemes)}>
+          <button className={`arrow__container--${showThemes ? 'down' : 'up'}`} aria-label={`${showThemes ? 'Hide color themes' : 'Show color themes'}`} onClick={() => setShowThemes(!showThemes)}>
             <Arrow className={`arrow arrow--${showThemes ? 'down' : 'up'}`} />
           </button>
           {
             showThemes ?
             <div className='flex justify-between w-full'>
-                <button onClick={() => setTheme({ color: 'blue' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 rounded-full blue-primary'></button>
-                <button onClick={() => setTheme({ color: 'red' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 rounded-full red-primary'></button>
-                <button onClick={() => setTheme({ color: 'dark' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 rounded-full dark-primary'></button>
-                <button onClick={() => setTheme({ color: 'green' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 rounded-full green-primary'></button>
+                <button onClick={() => setTheme({ color: 'blue' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 hover:saturate-150 rounded-full blue-primary'></button>
+                <button onClick={() => setTheme({ color: 'red' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 hover:saturate-150 rounded-full red-primary'></button>
+                <button onClick={() => setTheme({ color: 'dark' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 hover:saturate-150 rounded-full dark-primary'></button>
+                <button onClick={() => setTheme({ color: 'green' })} type='button' className='button--theme shadow-custom active:shadow-active w-60 h-60 hover:saturate-150 rounded-full green-primary'></button>
             </div>
             : <></>
           }
