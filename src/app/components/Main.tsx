a048dae53c7f5d00f12fc12a8cb7fc4963f32718
ferutizi <fer.utizi@gmail.com>
2024-01-26 14:40:24 -0300

    fixed Settings section max-height

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -65,75 +65,75 @@ export default function Main() {
             </button>
           </div>
         </section>
-      {modal && 
-        <section className="w-screen h-screen fixed z-20 bg-transparent">
-          <form onSubmit={handleSubmit} className={`flex flex-col text-lg ${color}-primary modal w-80 p-4 h-5/6 rounded-lg justify-between`}>
-            <article> {/* Header */}
-              <div onClick={() => {setModal(false), setTimerValue(initialTimer)}} className={`absolute right-4 py-0 px-2 rounded-full text-white cursor-pointer hover:bg-white hover:text-black transition-all ease-in duration-300`}>
-                <span>X</span>
-              </div>
-              <h2 className="text-white text-lg">Settings</h2>
-            </article>
-            <div className="flex flex-col gap-6"> {/* Options */}
-              <article className= "flex flex-col gap-4">
-                <div>
-                  <h2 className="text-white text-center text-xl pl-0">Timer</h2>
-                  <hr style={{borderColor: "white"}}></hr>
+        {modal && 
+          <section className="w-screen h-full max-h-hd fixed z-20 bg-transparent">
+            <form onSubmit={handleSubmit} className={`flex flex-col text-lg ${color}-primary modal w-80 p-4 h-5/6 rounded-lg justify-between`}>
+              <article> {/* Header */}
+                <div onClick={() => {setModal(false), setTimerValue(initialTimer)}} className={`absolute right-4 py-0 px-2 rounded-full text-white cursor-pointer hover:bg-white hover:text-black transition-all ease-in duration-300`}>
+                  <span>X</span>
                 </div>
-                <div className="flex flex-col items-center gap-4">
-                    <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Pomodoros
-                      <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} min={1} max={120} />
-                    </label>                  
-                    <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Breaks
-                      <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="break" type="number" value={timerValue.break} onChange={handleChange} min={1} max={120} />
-                    </label>                  
-                    <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Long breaks
-                      <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} min={1} max={120} />
-                    </label>                  
-                </div>
+                <h2 className="text-white text-lg">Settings</h2>
               </article>
-              <article className= "flex flex-col gap-4"> 
-                <div>
-                  <h2 className="text-white text-center text-xl pl-0">Sounds</h2>
-                  <hr style={{borderColor: "white"}}></hr>
-                </div>
-                <div className="flex flex-col items-center gap-2">
-                  <div className={`flex items-center p-2 ${color}-primary`}>
-                    <p className="flex items-center justify-between w-44 text-base text-white focus-within:text-lg transition-all ease-in duration-300">Play alarm</p>
-                    <button
-                      className={`w-20 h-8 text-xl text-black rounded text-center ${color}-light outline-none focus:bg-white`}
-                      type="button"
-                      onClick={() => setSoundOn(!soundOn)}
-                    >
-                      {soundOn ? 'ON' : 'OFF'}
-                    </button>
+              <div className="flex flex-col gap-6"> {/* Options */}
+                <article className= "flex flex-col gap-4">
+                  <div>
+                    <h2 className="text-white text-center text-xl pl-0">Timer</h2>
+                    <hr style={{borderColor: "white"}}></hr>
                   </div>
-                  <div className="flex items-center justify-between w-64 text-base text-black transition-all ease-in duration-300">
-                    <select disabled={!soundOn} className={`w-40 h-8 text-xl text-black rounded ${color}-light outline-none focus:bg-white`} onChange={(e) => setAlarmSound(e.target.value)} defaultValue={'note'}>
-                      <option value='alarm'>Alarm</option>
-                      <option value='alarmDouble'>Alarm Double</option>
-                      <option value='bells'>Bells</option>
-                      <option value='longBells'>Long Bells</option>
-                      <option value='complete'>Complete</option>
-                      <option value='note'>note</option>
-                    </select>
-                    <button disabled={!soundOn} type="button" className={`w-20 h-8 text-xl text-black rounded text-center ${color}-light outline-none focus:bg-white ${!soundOn && 'opacity-70'}`} onClick={() => playSound(alarmSound)}>Play</button>
+                  <div className="flex flex-col items-center gap-4">
+                      <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Pomodoros
+                        <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="pomodoro" type="number" value={timerValue.pomodoro} onChange={handleChange} min={1} max={120} />
+                      </label>                  
+                      <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Breaks
+                        <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="break" type="number" value={timerValue.break} onChange={handleChange} min={1} max={120} />
+                      </label>                  
+                      <label className="flex items-center justify-between w-64 text-base text-white focus-within:text-lg transition-all ease-in duration-300"> Long breaks
+                        <input className={`w-20 h-8 text-xl text-black rounded text-center ps-3 ${color}-light outline-none focus:bg-white`} name="longBreak" type="number" value={timerValue.longBreak} onChange={handleChange} min={1} max={120} />
+                      </label>                  
                   </div>
-                </div>
-              </article>
-            </div> {/* Footer */}
-            <button type="submit" className={`p-2 rounded-lg ${color}-light font-bold text-black shadow-custom active:shadow-active hover:bg-white transition-all ease-out 2seg duration-700`}>
-              Save changes
-            </button>
-          </form>
-        </section>
-      }
-      <Timer
-        pomodoroMinutes={pomodoroMinutes}
-        breakMinutes={breakMinutes}
-        longBreakMinutes={longBreakMinutes}
-        alarmSound={alarmSound}
-      />
+                </article>
+                <article className= "flex flex-col gap-4"> 
+                  <div>
+                    <h2 className="text-white text-center text-xl pl-0">Sounds</h2>
+                    <hr style={{borderColor: "white"}}></hr>
+                  </div>
+                  <div className="flex flex-col items-center gap-2">
+                    <div className={`flex items-center p-2 ${color}-primary`}>
+                      <p className="flex items-center justify-between w-44 text-base text-white focus-within:text-lg transition-all ease-in duration-300">Play alarm</p>
+                      <button
+                        className={`w-20 h-8 text-xl text-black rounded text-center ${color}-light outline-none focus:bg-white`}
+                        type="button"
+                        onClick={() => setSoundOn(!soundOn)}
+                      >
+                        {soundOn ? 'ON' : 'OFF'}
+                      </button>
+                    </div>
+                    <div className="flex items-center justify-between w-64 text-base text-black transition-all ease-in duration-300">
+                      <select disabled={!soundOn} className={`w-40 h-8 text-xl text-black rounded ${color}-light outline-none focus:bg-white`} onChange={(e) => setAlarmSound(e.target.value)} defaultValue={'note'}>
+                        <option value='alarm'>Alarm</option>
+                        <option value='alarmDouble'>Alarm Double</option>
+                        <option value='bells'>Bells</option>
+                        <option value='longBells'>Long Bells</option>
+                        <option value='complete'>Complete</option>
+                        <option value='note'>Note</option>
+                      </select>
+                      <button disabled={!soundOn} type="button" className={`w-20 h-8 text-xl text-black rounded text-center ${color}-light outline-none focus:bg-white ${!soundOn && 'opacity-70'}`} onClick={() => playSound(alarmSound)}>Play</button>
+                    </div>
+                  </div>
+                </article>
+              </div> {/* Footer */}
+              <button type="submit" className={`p-2 rounded-lg ${color}-light font-bold text-black shadow-custom active:shadow-active hover:bg-white transition-all ease-out 2seg duration-700`}>
+                Save changes
+              </button>
+            </form>
+          </section>
+        }
+        <Timer
+          pomodoroMinutes={pomodoroMinutes}
+          breakMinutes={breakMinutes}
+          longBreakMinutes={longBreakMinutes}
+          alarmSound={alarmSound}
+        />
       </div>
     </main>
   );
